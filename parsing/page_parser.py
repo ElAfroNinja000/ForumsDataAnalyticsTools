2310e2f1ae5803cb50a8e9ede301f261581f16ee
AfroNinjaaaaaaaa <m1105.108.megaman@gmail.com>
2023-02-04 01:24:51 +0100

    Added Request, Post and Comment parsing classes

--- parsing/page_parser.py
+++ parsing/page_parser.py
@@ -9,10 +9,10 @@ HEADERS = {
 class PageParser:
     def __init__(self, url: str):
         page_raw_data  = HeadlessWebDriver().get_raw_data_from_page(url)
-        self.page_html_data = BeautifulSoup(page_raw_data, "html.parser")
+        self.content = BeautifulSoup(page_raw_data, "html.parser")
 
     def get_elements(self, tag: str, class_: str):
-        return self.page_html_data.find_all(tag, class_=class_)
+        return self.content.find_all(tag, class_=class_)
 
     def get_element(self, tag: str, class_: str):
-        return self.page_html_data.find(tag, class_=class_)
+        return self.content.find(tag, class_=class_)
